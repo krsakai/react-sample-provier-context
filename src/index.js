aa93d99ca4d130d2e8978336994bbd4e6ea02ef3
酒井邦也 <k_sakai@kuniya-work.com>
2021-11-01 03:53:42 +0900

    Providerパターンでのカウンター処理を追加

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CounterContext from './contexts/counter'
+import Counter from './component/counter'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.state = {
+      count: 0,
+      increment: this.increment,
+      decrement: this.decrement
+    }
+  }
+
+  increment() {
+    this.setState({count: this.state.count + 1})
+  }
+
+  decrement() {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <CounterContext.Provider value={this.state}>
+        <Counter/>
+      </CounterContext.Provider>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
